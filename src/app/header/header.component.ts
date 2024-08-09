e9de6808e868e50b89e22e179abffebbad6d328a
Akshay <akshay@cloudrevelinnovation.com>
2024-08-09 16:37:10 +0530

    header for mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ConfettiService } from '../services/confetti.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(public confettiService: ConfettiService) { }
 
 }
