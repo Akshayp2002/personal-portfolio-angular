c134bd72af2a4180718ef4e4df4e0a6a4938d50f
Akshay <akshay@cloudrevelinnovation.com>
2024-09-23 16:00:00 +0530

    open close the menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ConfettiService } from '../services/confetti.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive,NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   constructor(public confettiService: ConfettiService) { }
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
