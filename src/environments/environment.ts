5e203acab2cbfb301ef5815a92e99f632e392fed
Akshay <84228768+Akshayp2002@users.noreply.github.com>
2025-11-25 18:53:09 +0530

    Change environment variable access method

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  spaceId: import.meta.env['NG_APP_SPACE_ID'],
-  accessToken: import.meta.env['NG_APP_ACCESS_TOKEN']
+  spaceId: process.env.NG_APP_SPACE_ID,
+  accessToken: process.env.NG_APP_ACCESS_TOKEN,
 };
